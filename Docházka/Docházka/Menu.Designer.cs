5adf4bf26d362a44ef1f589a09a603b9d99c3313
Dimitros617 <frolik.dominik@gmail.com>
2019-01-08 19:54:21 +0100

    Začátek

---
+++ Docházka/Docházka/Menu.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Docházka
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.zavrit = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(218)))), ((int)(((byte)(219)))));
+            this.label1.Location = new System.Drawing.Point(-3, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(805, 26);
+            this.label1.TabIndex = 1;
+            // 
+            // zavrit
+            // 
+            this.zavrit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(188)))), ((int)(((byte)(219)))));
+            this.zavrit.Cursor = System.Windows.Forms.Cursors.Default;
+            this.zavrit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.zavrit.Location = new System.Drawing.Point(773, -1);
+            this.zavrit.Name = "zavrit";
+            this.zavrit.Size = new System.Drawing.Size(28, 23);
+            this.zavrit.TabIndex = 2;
+            this.zavrit.Text = "X";
+            this.zavrit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.zavrit.Click += new System.EventHandler(this.zavrit_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(188)))), ((int)(((byte)(219)))));
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(741, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "_";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.zavrit);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu";
+            this.Text = "Docházka";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label zavrit;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Docházka/Docházka/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Docházka
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zavrit_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Opravdu si přejete program ukončit ?", "Zavřít ?", MessageBoxButtons.YesNo);
+            if (res == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
